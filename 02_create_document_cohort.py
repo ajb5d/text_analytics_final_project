9a2c5d68d7acb8b95f0276d5f94eb760fca714ff
Andrew Barros <ajb5d@Virginia.EDU>
2024-03-21 10:00:41 -0400

    subset the articles to those with easily available text

--- 02_create_document_cohort.py
+++ 02_create_document_cohort.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
 import pandas as pd
 from pathlib import Path
+from tqdm import tqdm
 
 DATA_DIR = Path("data")
 JOURNALS = ["jamia_open", "jamia"]
-ARTICLES_PER_YEAR = 20
+SUBSET_SIZE = 250
+
+## To get the article manifests from the public s3 bucket:
+# aws --no-sign-request s3 cp s3://pmc-oa-opendata/author_manuscript/xml/metadata/csv/author_manuscript.filelist.csv data/ 
+# aws --no-sign-request s3 cp s3://pmc-oa-opendata/oa_noncomm/xml/metadata/csv/oa_noncomm.filelist.csv data/
+# aws --no-sign-request s3 cp s3://pmc-oa-opendata/oa_comm/xml/metadata/csv/oa_comm.filelist.csv data/
+
+if not (DATA_DIR / "article_manifests.parquet").exists():
+    article_manifests = []
+    for subset in tqdm(["author_manuscript", "oa_noncomm", "oa_comm"]):
+        article_manifests.append(
+            pd.read_csv(
+                DATA_DIR / f"{subset}.filelist.csv",
+                usecols=["AccessionID", "Key", "PMID"]
+            )
+        )
+    pd.concat(article_manifests).to_parquet(DATA_DIR / "article_manifests.parquet")
+
+article_manifests = pd.read_parquet(DATA_DIR / "article_manifests.parquet").set_index("AccessionID")
 
 all_articles = [pd.read_parquet(DATA_DIR / f"{x}.parquet") for x in JOURNALS]
 all_articles = pd.concat(all_articles)
@@ -12,12 +31,11 @@ all_articles['sortdate'] = pd.to_datetime(all_articles['sortdate'])
 all_articles['pubyear'] = all_articles['sortdate'].dt.year
 
 print(f"Total articles: {len(all_articles)}")
-all_articles = all_articles[all_articles['pubyear'].between(2000, 2023)]
-print(f"Total articles (2000-2023): {len(all_articles)}")
+all_articles = all_articles.merge(article_manifests, left_on="pmcid", right_index=True)
+print(f"Total articles available in download subset: {len(all_articles)}")
 
-year_span = list(range(all_articles['pubyear'].min(), all_articles['pubyear'].max()+1))
-
-sampled_df = all_articles.groupby('pubyear').sample(n=ARTICLES_PER_YEAR, random_state=0)
+sampled_df = all_articles.sample(n=SUBSET_SIZE, random_state=0)
 print(f"Sampled articles: {len(sampled_df)}")
 
+all_articles.to_parquet(DATA_DIR / "all_articles.parquet")
 sampled_df.to_parquet(DATA_DIR / "sampled_articles.parquet")
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 1. `01_build_article_list.py` uses the [NCBI Entrez APIs](https://www.ncbi.nlm.nih.gov/books/NBK25501/) to build a list of articles from candidate journals that are available in [Pub Med Central](https://www.ncbi.nlm.nih.gov/pmc/). 
 
-1. `02_create_document_cohort.py` uses the article lists from the previous step and samples the articles stratified by publication year.
+1. `02_create_document_cohort.py` uses the article lists from the previous step, merges it with the metadata of available articles, and samples the articles.
